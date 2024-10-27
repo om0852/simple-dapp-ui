e63d1d69f18065d02c4a5aad2e4e08c9c9dc08a2
om salunke <omsaunke343@gmail.com>
2024-10-27 22:26:10 +0530

    front end ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    const loadProvicer = async () => {
+      console.log(window.web3);
+      console.log(window?.ethereum);
+
+      // console.log(window>.ethereum.target);
+    };
+
+    return () => {
+      loadProvicer();
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <div className="card-body">Funding</div>
+      </div>{" "}
+      <div className="card">
+        <div className="card-body">Balance:20ETH</div>
+      </div>{" "}
+      <div className="card">
+        <div className="card-body">Account:0000x0000</div>
+      </div>{" "}
+      <button
+        type="button"
+        onClick={async () => {
+          const accounts = await window?.ethereum.request({
+            method: "eth_requestAccounts",
+          });
+          console.log(accounts[0]);
+        }}
+        className="btn btn-success"
+      >
+        Connect to metamask
+      </button>
+      <button type="button" className="btn btn-success">
+        Transfer
+      </button>
+      <button type="button" className="btn btn-primary">
+        Withdraw
+      </button>
     </div>
   );
 }
